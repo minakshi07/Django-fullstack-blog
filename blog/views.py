b8db9d77bb66ebf482a3835feb9f470a10f4d53f
Minakshi Kudalkar <42650284+minakshi07@users.noreply.github.com>
2020-03-14 14:06:57 +0530

    Frontend with models ready

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Post
+posts=[
+    {
+        'author':' Minakshi Kudalkar',
+        'title':'Chicas Blog',
+        'content':'First blog content is this',
+        'date_posted':'August 16,2019'
+    },
+    {
+        'author':'Shashank Gupta',
+        'title':'Chicos Blog',
+        'content':'Second blog content is this. Hey you!',
+        'date_posted':'August 17,2019'
+    }
+]
+
+
+def home(request):
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
+
+def about(request):
+    return render(request, 'blog/about.html',{'title':'About Page'})
